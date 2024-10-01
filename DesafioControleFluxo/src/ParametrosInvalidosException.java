d2682dd543afe65947eae3a81529c4136d6f63d3
lnrd_g <godinho1600@gmail.com>
2024-10-01 12:29:28 -0300

    Controle de Fluxo - Desafio

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    
+    public ParametrosInvalidosException() {
+        super("O segundo parametro deve ser maior que o primeiro");
+    }
+}
